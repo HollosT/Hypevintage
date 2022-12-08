6610394a487e2341b78730a8f1deedd7d1c64b51
HollosT <hollos.tamas@ravensoft.hu>
2022-12-08 19:45:15 +0100

    Filter continue

--- assets/application.js
+++ assets/application.js
@@ -12,12 +12,9 @@ if(filterFormBtn != null) {
     let checkedType;
 
     checkBoxes.forEach((checkBox, i) => {
-        
+
         checkBox.addEventListener('change', e => {
-            if(e.target.checked) {
-                checkedType = e.target.value
-                console.log(checkedType);
-            }
+            checkedType = e.target.value
         })
     })
 
@@ -31,11 +28,12 @@ if(filterFormBtn != null) {
         })
         console.log(url);
 
-        filterFromBtn.innerHTML = 'disabled'
+        filterFormBtn.innerHTML = 'disabled'
 
         // https://hypevintage-aalborg.myshopify.com/collections/types?q=Accessories
-        const newUrl = `${url}types?q=${checked[0]}`
- 
+        const newUrl = `${url}types?q=${checkedType}`;
+        
+        window.location.href = newUrl;
     })
 
   
