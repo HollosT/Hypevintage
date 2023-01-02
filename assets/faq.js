9c1b9c9ebba1dbea1b63fe2462de095606dc7c46
HollosT <hollos.tamas@ravensoft.hu>
2023-01-02 17:30:33 +0100

    Q&A

---
+++ assets/faq.js
@@ -0,0 +1,63 @@
+const faqSection = document.querySelector('#faqSection');
+
+if(faqSection != null) {
+    const container = document.querySelector('.faq-container');
+    const answers = document.querySelectorAll('.answer');
+    const arrows = document.querySelectorAll('.arrow');
+    const faqs = document.querySelectorAll('.faq')
+
+    container.addEventListener('click', (e) => {
+        const clicked = e.target.closest('.arrow')
+        if (!clicked) return ;
+        const id = clicked.dataset.faq
+        
+        if(clicked.dataset.open === 'false') {
+            open(id)
+
+        } else if(clicked.dataset.open === 'true') {
+            close(clicked)
+        }
+    })
+
+
+    const open = (id) => {
+
+        faqs.forEach(faq => {
+            if(faq.dataset.faq === id) {
+                faq.classList.add('faq-active');
+            } else {
+                faq.classList.remove('faq-active');
+
+            }
+
+        })
+
+
+        answers.forEach(answer => {
+            if(answer.dataset.faq === id) {
+                answer.classList.remove('hide')
+            } else {
+                answer.classList.add('hide')
+            }
+        });
+
+        arrows.forEach(arrow => {
+            if(arrow.dataset.faq === id) {
+                arrow.classList.add('active-arrow');
+                arrow.dataset.open = 'true'
+            } else {
+                arrow.dataset.open = 'false'
+                arrow.classList.remove('active-arrow')
+
+            }
+        });
+        
+    }
+
+    const close = (clicked) => {
+        arrows.forEach(arrow => arrow.classList.remove('active-arrow'));
+        faqs.forEach(faq => faq.classList.remove('faq-active'));
+        clicked.dataset.open = 'false'
+        answers.forEach(answer => answer.classList.add('hide'))
+    }
+}
--- assets/slideshow.js
+++ assets/slideshow.js
@@ -3,27 +3,31 @@ const slideShow = document.querySelector('#slideShow');
 if(slideShow != null) {
     const slides = document.querySelectorAll('.slide');
     const tabContainer = document.querySelector('.tab-title-container');
-
+    const tabs = document.querySelectorAll('.tab')
 
     // Setting the position of the images
     const goToSlide = function(curIndex) {
+        tabs.forEach(tab => tab.classList.remove('active-slide'))
+        tabs[curIndex].classList.add('active-slide')
+       
         slides.forEach((slide ,i ) => {
             slide.style.transform = `translateX(${100 * (i - curIndex)}%)`
         })
     }
 
     
-    
     tabContainer.addEventListener('click', e => {
-        console.log('hello');
         const clicked = e.target.closest('.tab');
 
         if(!clicked) return;
-       
-        goToSlide(+clicked.dataset.tab)
+
+
+        const curIndex = +clicked.dataset.tab - 1;
+        goToSlide(curIndex)
     })
     
     const init = () => {
+        
         goToSlide(0)
     };
 
