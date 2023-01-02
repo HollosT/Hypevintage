de873137097f76c6e2031e97c7393604d094e2d1
HollosT <hollos.tamas@ravensoft.hu>
2023-01-02 08:41:45 +0100

    init slideshow

--- assets/mobile-category.js
+++ assets/mobile-category.js
@@ -3,22 +3,39 @@ const mobileCategory = document.querySelector('#mobileCategory');
 if(mobileCategory != null) {
     const container = document.querySelector('.mobile-categories-container');
     const subCategories = document.querySelectorAll('.sub-category')
-    const mainCategories = document .querySelectorAll('.main-category');
-    container.addEventListener('click', (e) => {
-        e.preventDefault();
+    const mainCategories = document.querySelectorAll('.main-category');
+    const backIcons = document.querySelectorAll('.category-icon')
 
+    container.addEventListener('click', (e) => {
         const clicked = e.target.closest('.main-category')
         if (!clicked) return ;
-        const i = +clicked.dataset.link - 1
+        const i = +clicked.dataset.link - 1;
 
-        const active = subCategories[i]
-        
-        mainCategories.forEach(main => main.classList.remove('active-main'))
-        clicked.classList.add('active-main')
+        const active = subCategories[i];
+        const backActive = backIcons[i];
 
-        subCategories.forEach(sub => sub.classList.add('hide'))
-        active.classList.remove('hide')
+        resetCategory();
+
+        backActive.classList.remove('hide');
+        clicked.classList.add('active-main');
+        active.classList.remove('hide');
+
+        addCloseHandler(backActive);
   
     })
+
+    const addCloseHandler = (active) => {
+        active.addEventListener('click', () => {
+            resetCategory();
+        })
+    }
+    
+
+    const resetCategory = () => {
+        backIcons.forEach(icon => icon.classList.add('hide'));
+        mainCategories.forEach(main => main.classList.remove('active-main'));
+        subCategories.forEach(sub => sub.classList.add('hide'));
+    }
+
 
 }
