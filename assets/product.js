f3b65b27ec0c7eeca77a1ec1c2b858af196f5d18
HollosT <hollos.tamas@ravensoft.hu>
2023-01-10 11:18:48 +0100

    finsih product image

--- assets/product.js
+++ assets/product.js
@@ -26,4 +26,55 @@ if(productPage != null) {
             break;
     }
 
+
+    const productSubImg = document.querySelector('.product-sub-image');
+    const productMainImg = document.querySelector('.product-main-img');
+    const imgContainer = document.querySelector('.product-image-container')
+
+    // 
+    productSubImg.addEventListener('click', (e) => {
+        
+        // Set the sub image
+        productSubImg.classList.add('grow-image')
+
+        // Set the main image
+        productMainImg.classList.add('product-sub-image')
+        productMainImg.classList.remove('product-main-img')
+
+        
+        setTimeout(() => {
+            productSubImg.classList.remove('product-sub-image')
+            productSubImg.classList.remove('grow-image')
+            productSubImg.classList.add('product-main-img')
+            
+
+            addEventHandlerToMainImage()
+        }, 250)
+
+       
+    })
+
+    const addEventHandlerToMainImage = () => {
+
+        productMainImg.addEventListener('click', (e) => {
+            
+            // Set the sub image
+            productMainImg.classList.add('grow-image')
+
+            // Set the main image
+            productSubImg.classList.add('product-sub-image')
+            productSubImg.classList.remove('product-main-img')
+
+            
+            setTimeout(() => {
+                productMainImg.classList.remove('product-sub-image')
+                productMainImg.classList.remove('grow-image')
+                productMainImg.classList.add('product-main-img')
+                
+            }, 250)
+        })
+    }
+
+
+
 }
